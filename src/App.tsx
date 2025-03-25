114090483b61680141956e36d550930c289dbe3b
Lyaguh NoteBook <artyom07206@gmail.com>
2025-03-25 20:34:50 +0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Button from "./components/ui/Button";
-import Input from "./components/ui/Input";
-
 export default function App() {
   return (
     <>
-      <Button className="mr-6">Применить</Button>
-      <Button appearance="big">Оформить</Button>
-      <Input placeholder="Email" type="email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
     </>
   );
 }
